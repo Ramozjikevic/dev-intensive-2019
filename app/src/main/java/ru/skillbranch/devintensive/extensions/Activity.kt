35a82a288c23de1c6ea5e3ba044641464ea19e30
Vlad <v.a.chikishev@tmn2.etagi.com>
2020-04-12 02:34:14 +0500

    add 6, 7 example

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -13,3 +14,16 @@ fun Activity.hideKeyboard() {
     }
     inputMethodManager.hideSoftInputFromWindow(focusedView.windowToken, 0)
 }
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val softKeyboardHeight = 128
+
+    val rect = Rect()
+    window.decorView.getWindowVisibleDisplayFrame(rect)
+    val dm = window.decorView.resources.displayMetrics
+    val heightDiff = window.decorView.bottom - rect.bottom
+
+    return heightDiff > softKeyboardHeight * dm.density
+}
+
+fun Activity.isKeyboardClosed(): Boolean = !isKeyboardOpen()
